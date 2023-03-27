1fff94c2245490d2988f0c513bcfa9fca3bc233e
junyojeo <jjy8479@gmail.com>
2023-03-27 13:13:27 +0900

    1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:59:23 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/03/23 18:51:22 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:00:35 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,62 @@
 
 void	ft_perror(char *str)
 {
-	ft_putstr_fd(str, 1);
-	ft_putstr_fd("\n", 1);
+	perror(str);
 	exit(1);
 }
 
-void	parse(t_time *time, char **ar)
+void	parse(t_time *time, int ac, char **ar)
 {
 	time->number_of_philosophers = ft_atoi(ar[1]);
 	time->fork = ft_atoi(ar[1]);
 	time->time_to_die = ft_atoi(ar[2]);
 	time->time_to_eat = ft_atoi(ar[3]);
 	time->time_to_sleep = ft_atoi(ar[4]);
-	time->number_of_times_each_philosopher_must_eat = ft_atoi(ar[5]);
+	if (ac == 5)
+		time->number_of_times_each_philosopher_must_eat = ft_atoi(ar[5]);
 }
 
-void	philosophers(t_time *time, char **ar)
+t_philo	ft_add_philo(t_time *time, int i);
 {
-	struct timeval mytime;
+	pthread_t	thread_t;
+	int	a = 100;
+	int	status;
+	
+	if (pthread_create(&thread_t, NULL, func, (void *)&a) < 0)
+		ft_perror("thread create error:");
+	pthread_join(thread_t, (void **)&status);
+	t_philo
+	int	i;
+	
+	i = 0;
+	while (i < time->number_of_philosophers)
+		ft_lstnew(pthread_create(i, ));
+}
 
-	parse(time, ar);
+void	philosophers(t_time *time, int ac, char **ar)
+{
+	struct timeval	mytime;
+	int				i;
+
+	parse(time, ac, ar);
+	i = -1;
+	while (++i < time->number_of_philosophers)
+	{
+		ft_add_philo(time);
+	}
 	gettimeofday(&mytime, NULL);
+	
+	pthread_create(thread, pthread_attr_t ,start_routine, arg);
+	
 }
 
 int	main(int ac, char **ar)
 {
-	t_time time;
+	t_time	time;
 
 	if (ac < 4 || 5 < ac)
 		ft_perror("Error: argv ");
-	philosophers(&time, ar);
-	if (ac == 5)
-	
+	philosophers(&time, ac, ar);
 	return (0);
 }
 
--- src/philosophers.h
+++ src/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:59:51 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/03/23 18:46:37 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:09:45 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,29 @@
 
 # include "libft/libft.h"
 # include <sys/time.h>
+# include <pthread.h>
 # include <stdio.h>
 
-typedef struct s_philosophers
+/* 시간 */
+//현재 시간을 가져오고 시스템 시간을 설정 int gettimeofday(struct timeval *tv, struct timezone *tz);
+/* 프로세스 */
+//쓰레드 생성 int pthread_create(pthread_t *thread, pthread_attr_t *attr, void * (*start_routine)(void *), void * arg);
+//쓰레드를 분리 상태로 만듬 int pthread_detach(pthread_t th);
+//쓰레드가 종료되는 걸 기다림 int pthread_join(pthread_t th, void **thread_return);
+/* 뮤텍스 */
+//뮤텍스 초기화 int pthread_mutex_init(pthread_mutex_t * mutex, const pthread_mutex_attr *attr);
+//뮤텍스 파기 및  초기화 int pthread_mutex_destroy(pthread_mutex_t *mutex);
+//뮤텍스를 잠금 int pthread_mutex_lock(pthread_mutex_t *mutex);
+//int pthread_mutex_trylock(pthread_mutex_t *mutex);
+//뮤텍스 해제int pthread_mutex_unlock(pthread_mutex_t *mutex);
+//int pthread_mutex_destroy(pthread_mutex_t *mutex);
+
+typedef struct s_philo
 {
+	int	n_philo;
 	int	l_fork;
 	int	r_fork;
-}		t_philosophers;
+}		t_philo;
 
 typedef struct s_state
 {
