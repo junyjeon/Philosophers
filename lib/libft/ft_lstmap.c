b8058914888ddca27d8ff593635dca500dda1f9a
junyojeo <jjy8479@gmail.com>
2023-03-30 03:37:48 +0900

    1

--- lib/libft/ft_lstmap.c
+++ lib/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 03:50:59 by marvin            #+#    #+#             */
-/*   Updated: 2023/03/14 05:30:05 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/03/30 03:36:46 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*newhead;
+	void	*tmp;
 
 	newhead = NULL;
 	while (lst)
 	{
-		new = ft_lstnew(f(lst->content));
+		tmp = f(lst->content);
+		new = ft_lstnew(tmp);
 		if (!new)
 		{
 			ft_lstclear(&newhead, del);
+			del(tmp);
 			return ((void *)(0));
 		}
 		ft_lstadd_back(&newhead, new);
