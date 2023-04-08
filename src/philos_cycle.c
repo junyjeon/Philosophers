b787f214e2810475ceec10ed0b0cbf6fd04821cf
junyojeo <jjy8479@gmail.com>
2023-04-08 15:34:20 +0900

    23

--- src/philos_cycle.c
+++ src/philos_cycle.c
@@ -6,24 +6,25 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 02:18:38 by junyojeo          #+#    #+#             */
-/*   Updated: 2023/04/08 15:32:57 by junyojeo         ###   ########.fr       */
+/*   Updated: 2023/04/08 15:34:17 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static void	print_mutex(char *str, int now, t_philo *philo)
+static int	print_mutex(char *str, int now, t_philo *philo)
 {
 	pthread_mutex_lock(philo->info->end_flag_mutex);
 	if (philo->info->end_flag == 1)
 	{
 		pthread_mutex_unlock(philo->info->end_flag_mutex);
-		break;
+		return (0);
 	}
 	pthread_mutex_unlock(philo->info->end_flag_mutex);
 	pthread_mutex_lock(philo->info->print_mutex);
 	printf("%d %d %s", now, philo->num, str);
 	pthread_mutex_unlock(philo->info->print_mutex);
+	return (1);
 }
 
 static int	ft_usleep(t_philo *philo, int now, int time_to_spend)
@@ -52,14 +53,17 @@ static int	eating(t_philo *philo)
 
 	now = timer(philo, 0);
 	pthread_mutex_lock(&philo->info->fork[philo->num - 1]);
-	print_mutex("has taken a fork\n", now, philo);
+	if (!print_mutex("has taken a fork\n", now, philo))
+		return (0);
 	now = timer(philo, 0);
 	pthread_mutex_lock(&philo->info->fork[(philo->num) % philo->info->number_of_philosophers]);
-	print_mutex("has taken a fork\n", now, philo);;
+	if (!print_mutex("has taken a fork\n", now, philo))
+		return (0);
 	now = timer(philo, 0);
 	philo->eat_time = now;
 	philo->eat_cnt++;
-	print_mutex("is eating\n", now, philo);
+	if (!print_mutex("is eating\n", now, philo))
+		return (0);
 	pthread_mutex_unlock(&philo->info->fork[philo->num - 1]);
 	pthread_mutex_unlock(&philo->info->fork[(philo->num) % philo->info->number_of_philosophers]);
 	if (!ft_usleep(philo, now, philo->info->time_to_eat))
@@ -84,10 +88,12 @@ static void	philos_cycle(t_philo *philo)
 			pthread_mutex_unlock(philo->info->end_flag_mutex);
 			break ;
 		}
-		print_mutex("is sleeping\n", timer(philo, 0), philo);
+		if (!print_mutex("is sleeping\n", timer(philo, 0), philo))
+			break ;
 		if (!ft_usleep(philo, timer(philo, 0), philo->info->time_to_sleep))
 			break ;
-		print_mutex("is thinking\n", timer(philo, 0), philo);
+		if (!print_mutex("is thinking\n", timer(philo, 0), philo))
+			break ;
 	}
 }
 
